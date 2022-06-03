1474c9d1c8bcc23e3e55318cfddcc7b33dbc7fcc
Collins Sirwani <sircollo71@gmail.com>
2022-06-04 01:32:48 +0300

    Configure postgresql database settings

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'instagram_clone.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'collo',
+    'PASSWORD':'12345',
     }
 }
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
