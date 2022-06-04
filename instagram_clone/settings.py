b4aa4bce209ba7aa65a6dc2d5ecb55143b7b83c0
Collins Sirwani <sircollo71@gmail.com>
2022-06-04 17:35:35 +0300

    This will add crispy forms settings

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'instagram',
-    'bootstrap4'
+    'bootstrap4',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -130,4 +131,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK='bootstrap4'
 
