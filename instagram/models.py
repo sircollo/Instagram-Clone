d44641e6588b4021ba04a309ed84018f846df74a
Collins Sirwani <sircollo71@gmail.com>
2022-06-04 23:29:26 +0300

    Add image model class

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,19 @@
+from distutils.command.upload import upload
 from django.db import models
+from cloudinary.models import CloudinaryField
+import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Image(models.Model):
+  image = CloudinaryField('image')
+  name = models.CharField(max_length=30,default='image_name')
+  caption = models.CharField(max_length=30,default='')
+  upload_date = models.DateTimeField(auto_now_add=True, null=True)
+  owner = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+  
+  def __str__(self):
+    return self.name
+
+
+
+
