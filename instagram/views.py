b149be1095580252d87d4bb4f9834196b0eb0779
Collins Sirwani <sircollo71@gmail.com>
2022-06-04 22:28:51 +0300

    define view function and authenticate user

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
-
+from email import message
+from urllib import request
+from django.shortcuts import render,redirect
+from .forms import UserForm
+from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
 # Create your views here.
 
 def home(request):
-  return render(request, 'index.html')
+  if request.method == 'POST':
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    
+    user = authenticate(request, username=username,password=password)
+    
+    if user is not None:
+      login(request,user)
+      redirect('index')
+    else:
+      messages.info(request, 'Incorrect Username or password')
+
+  context = {}
+  return render(request, 'registration/login.html',context)
+
+  
+
