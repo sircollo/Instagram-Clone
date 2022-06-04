8af55cfb3e397b0d5e6daf64be80df2ad20ab0e0
Collins Sirwani <sircollo71@gmail.com>
2022-06-04 22:31:10 +0300

    Feat: define url path for registration

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,5 +1,14 @@
-from django.urls import re_path as url
+from atexit import register
+from django.urls import include, path, re_path as url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    url('$', views.home, name='home')
+    url('$', views.home, name='home'),
+    url('^register/',views.register, name='register'),
+    
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
