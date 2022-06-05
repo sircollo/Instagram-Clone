71f576827ca9e7791651bcfb26401ec370b33be2
Collins Sirwani <sircollo71@gmail.com>
2022-06-06 01:06:53 +0300

    Configure media files settings

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -129,7 +130,12 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+LOGIN_REDIRECT_URL = 'caseygram-home'
+LOGOUT_REDIRECT_URL = 'home'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
