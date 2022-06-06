6e6639caa074834ed32ae2d93e3be25604d082cc
Collins Sirwani <sircollo71@gmail.com>
2022-06-06 21:28:21 +0300

    Configure user configurations to read signals.py file

--- instagram/apps.py
+++ instagram/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class InstagramConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'instagram'
+
+
+class UserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'users'
+
+    # add this
+    def ready(self):
+        import instagram.signals 
