2369fd55beca033dea649b4735bbe73041430081
Collins Sirwani <sircollo71@gmail.com>
2022-06-07 11:44:55 +0300

    Fix: login redirect url fixed

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -171,7 +171,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-LOGIN_REDIRECT_URL = 'caseygram-home'
+LOGIN_REDIRECT_URL = ''
 LOGOUT_REDIRECT_URL = 'home'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
